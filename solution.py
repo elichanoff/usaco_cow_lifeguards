4d5e4dbd550c64e7e0e68efe70825c3a552d54c9
Eli Chanoff <eli.k.chanoff@gmail.com>
2023-08-25 15:00:46 -0700

    Adds instructive comments to code

--- solution.py
+++ solution.py
@@ -6,8 +6,11 @@ class Solution:
 
     def read_in(self):
         with open(self.input) as f:
+            ## The first line of the input file is the number of shifts
             num_shifts = int(f.readline())
             timepoints = []
+            ## For each subsequent line, split the string into start and end time point tuples,
+            ## convert the strings to integers, and put them in the timepoints list.
             for line in f:
                 timepoints_str = line.split()
                 timepoints_int = (int(timepoints_str[0]), int(timepoints_str[1]))
@@ -18,30 +21,47 @@ class Solution:
     def maximum_covered_time(self):
         flattened_shifts = []
         alone_time = {}
+        ## Split each shift into tuples (cow_n, start) and (cow_n, end).
+        ## Add tuples to the list flattened_shifts.
         for cow, shift in enumerate(self.shifts):
+            ## Also add to the dict alone_time key: value pairs for each cow.
+            ## Each cow starts with alone time of 0.
             alone_time[f'cow_{cow}'] = 0
             for timepoint in shift:
                 flattened_shifts.append((timepoint, f'cow_{cow}'))
 
+        ## Sort the flattened shifts
+        flattened_shifts.sort()
 
-        flattened_shifts.sort()  # Sort the shifts by their starting times
-
+        ## Keep track of which cows are working, the previously encountered time point, and the total time.
+        ## Here, we start these off based on the first time point in flattened_shifts.
         working = [flattened_shifts[0][1]]
         last = flattened_shifts[0][0]
         total_time = 0
 
+        ## Since we instantiated the above variables with the first time point in flattened shifts,
+        ## we will start iterating from the second time point.
         for timepoint in flattened_shifts[1:]:
+            ## Calculate time pass since previous time point
             time_passed = timepoint[0] - last
+            ## If a cow was working in that period, increment the total time.
             if len(working) > 0:
                 total_time += time_passed
+            ## If only one cow was working, increment that cow's alone time.
             if len(working) == 1:
                 alone_time[working[0]] += time_passed
+            ## If the cow in the current time point was already working, then their shift is over.
+            ## Remove that cow from working
             if timepoint[1] in working:
                 working.remove(timepoint[1])
+            ## Otherwise, their shift is starting. Add that cow to working.
             else:
                 working.append(timepoint[1])
+            ## Update the previously encountered endpoint
             last = timepoint[0]
 
+        ## The cow with the least alone time is the minimum value in the alone_time dict.
         least_alone_time = min(alone_time.values())
+        ## Finally, calculate the maximum covered time by subtracting the least alone time from the total time.
         max_covered_time = total_time - least_alone_time
         return max_covered_time
--- test.py
+++ test.py
@@ -6,8 +6,8 @@ import os
 
 path = "./test_data"
 
-
 def extract_test_data():
+    ## Retrieve the test data from the USACO website. Extract the zip file, and put the test data files into ./test_data.
     url = "http://www.usaco.org/current/data/lifeguards_silver_jan18.zip"
     resp = urlopen(url)
     myzip = ZipFile(BytesIO(resp.read()))
@@ -16,14 +16,18 @@ def extract_test_data():
 
 def test_solution():
     results = {}
+    ## There are 10 test cases, each with an input and an output file.
+    ## Since the test data files are named eg. '2.in', '2.out', we can iterate through range(1, 11) to hit all cases.
     for i in range(1, 11):
         input_path = f"/Users/eli/pet_projects/coding_minds_solution/test_data/{i}.in"
         output_path = f"/Users/eli/pet_projects/coding_minds_solution/test_data/{i}.out"
+        ## Instantiate Solution with the test input data.
         test_solution = Solution(input_path)
         print("solution response: ", test_solution.maximum_covered_time())
         print("Actual solution:")
         with open(output_path) as f:
             expected = int(f.readline())
+            ## Write the Solution output, the expected output, and if they match, into the results dict.
             results[f"test_{i}"] = {
                 "Actual": test_solution.maximum_covered_time(),
                 "Expected": expected,
@@ -33,6 +37,7 @@ def test_solution():
     return results
 
 def run():
+    ## Only retrieve the test data if ./test_data is empty.
     dir = os.listdir(path)
     if len(dir) == 0:
         print("Retrieving test data")
@@ -40,6 +45,7 @@ def run():
     else:
         print("Test data already found!")
     print("checking answers...")
+    ## Test Solution against all test cases, and print the results.
     test_results = test_solution()
     print(test_results)
 
